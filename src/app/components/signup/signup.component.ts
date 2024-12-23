104c25db33ca6cf6b0b22f8e1b71498ea213b305
Karim Debza <bwb-k.debza@fondespierre.com>
2024-12-23 18:03:56 +0100

    mettre en place la structure html css

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  public signupForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+ 
+}
